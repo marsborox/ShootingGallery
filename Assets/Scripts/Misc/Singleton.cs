d74cc4833ba56da5ba85268c8443e2ebe34e2c86
mierka_m <162184787+marsborox@users.noreply.github.com>
2025-02-25 23:51:50 +0100

    pre midnight commit singleton wrk

--- Assets/Scripts/Misc/Singleton.cs
+++ Assets/Scripts/Misc/Singleton.cs
@@ -2,17 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Singleton : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+public class Singleton<T> : MonoBehaviour where T : Singleton<T>
+{
+
+    private static T instance;
+
+    public static T Instance { get { return instance; } }
+    //it will be like semi private, acessible only to whoever directly inherits
+    protected virtual void Awake()
     {
-        
+        if (instance != null && this.gameObject != null)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {//instance needs to be casted as generic type
+            instance = (T)this;
+        }
+        DontDestroyOnLoad(gameObject);
     }
 }
+

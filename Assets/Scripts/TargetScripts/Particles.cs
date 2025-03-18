4acc20100c94beb3dc591eb301cffde1e9c5b6c5
mierka_m <162184787+marsborox@users.noreply.github.com>
2025-03-18 20:04:28 +0100

    Revert "tried to fix pooling must rollback"
    This reverts commit ad61918acf86806200e0cf8955da0c3a25af240c.

--- Assets/Scripts/TargetScripts/Particles.cs
+++ Assets/Scripts/TargetScripts/Particles.cs
@@ -3,14 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Pool;
 
-public class BleedParticlePool : MonoBehaviour
+public class Particles : MonoBehaviour
 {
     Target target;
     // deactivate after delay
-    [SerializeField] GameObject bleedParticle;
+    [SerializeField] BleedParticle bleedParticle;
 
     // stack-based ObjectPool available with Unity 2021 and above
-    public IObjectPool<GameObject> bleedObjectPoolPublic;
+    public IObjectPool<BleedParticle> bleedObjectPoolPublic;
 
     // throw an exception if we try to return an existing item, already in the pool
     [SerializeField] private bool collectionCheck = true;
@@ -21,35 +21,21 @@ public class BleedParticlePool : MonoBehaviour
 
     private void Awake()
     {
-            bleedObjectPoolPublic = new ObjectPool<GameObject>(CreateParticle,
+            bleedObjectPoolPublic = new ObjectPool<BleedParticle>(CreateParticle,
             OnGetFromPool, OnReleaseToPool, OnDestroyPooledObject,
             collectionCheck, defaultCapacity, maxSize);
     }
 
-    private void Start()
-    {
-
-        TestParticleSpawn();
-    }
-    void TestParticleSpawn()
-    {
-        GameObject particle = Instantiate(bleedParticle);
-        Instantiate(particle);
-        Debug.Log("bleedParticlePool particle created - test");
-    }
-
     // invoked when creating an item to populate the object pool
-    private GameObject CreateParticle()
+    private BleedParticle CreateParticle()
     {
-        Debug.Log("bleedParticlePool particle created");
-
-        GameObject bleedParticleInstance = Instantiate(bleedParticle.gameObject);
-        bleedParticleInstance.GetComponent<BleedParticle>().bleedObjectPoolPublic = bleedObjectPoolPublic;
+        BleedParticle bleedParticleInstance = Instantiate(bleedParticle);
+        bleedParticleInstance.bleedObjectPoolPublic = bleedObjectPoolPublic;
             
         return bleedParticleInstance;
     }
     // invoked when retrieving the next item from the object pool
-    public void OnGetFromPool(GameObject pooledObject)
+    public void OnGetFromPool(BleedParticle pooledObject)
     {
         pooledObject.gameObject.SetActive(true);
 
@@ -57,13 +43,13 @@ public class BleedParticlePool : MonoBehaviour
     }
 
     // invoked when returning an item to the object pool
-    private void OnReleaseToPool(GameObject pooledObject)
+    private void OnReleaseToPool(BleedParticle pooledObject)
     {
         pooledObject.gameObject.SetActive(false);
     }
 
     // invoked when we exceed the maximum number of pooled items (i.e. destroy the pooled object)
-    private void OnDestroyPooledObject(GameObject pooledObject)
+    private void OnDestroyPooledObject(BleedParticle pooledObject)
     {
         Destroy(pooledObject.gameObject);
     }

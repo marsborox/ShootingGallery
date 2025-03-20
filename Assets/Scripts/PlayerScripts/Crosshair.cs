b56e70aec17104f51157750640213c8223f9413b
marsborox <mierkamaros@gmail.com>
2025-03-20 21:19:04 +0100

    attempt to fix crosshair not scaling on pc/standalone

--- Assets/Scripts/PlayerScripts/Crosshair.cs
+++ Assets/Scripts/PlayerScripts/Crosshair.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Crosshair : MonoBehaviour
 {
-    [SerializeField] Sprite crosshairSprite;
+    //[SerializeField] Sprite crosshairSprite;
 
     public Texture2D crosshair;
     public Texture2D crosshairUpdate;
@@ -17,12 +17,14 @@ public class Crosshair : MonoBehaviour
     }
     void SetImg()
     {
-        cursorHotspot = new Vector2(crosshair.width / 2, crosshair.height / 2);
+        cursorHotspot = new Vector2(crosshair.width / 2, crosshair.height / 2);// so its set in centre of cursor object
+        //if we want like strtegy game cursor than its just w
         Cursor.SetCursor(crosshair, cursorHotspot, CursorMode.ForceSoftware);
     }
     void SetSize()
-    {
-        crosshair.width = crosshair.width * 2;
-        crosshair.height = crosshair.height * 2;
+    {// WORKS FOR HTML ONLY MUST FIX FOR PC
+        crosshair.width = crosshair.width*2;
+        crosshair.height = crosshair.height*2;
+        
     }
 }
